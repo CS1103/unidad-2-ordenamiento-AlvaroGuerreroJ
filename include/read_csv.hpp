c74aecee7804ea2e539cc8a57c36bacb2cef3440
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2019-10-01 15:54:54 -0500

    Check if file is open

--- include/read_csv.hpp
+++ include/read_csv.hpp
@@ -2,6 +2,8 @@
 #define GUARD_READ_CSV_HPP
 
 #include <fstream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -11,6 +13,12 @@ template<template<class...> class retcon = std::vector>
 retcon<std::vector<std::string>> read_csv(std::string filename)
 {
     std::ifstream f(filename);
+    if (!f.is_open())
+    {
+        std::ostringstream os;
+        os << "\"" << filename << "\"" << " can't be opened";
+        throw std::domain_error(os.str());
+    }
     aria::csv::CsvParser parser(f);
 
     retcon<std::vector<std::string>> ret(parser.begin(), parser.end());
